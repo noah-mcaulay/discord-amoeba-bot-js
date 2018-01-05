e4324ea4e37142bcb02c684a2b7aac81304eddb1
Noah Mcaulay <noah.mcaulay@gmail.com>
2018-01-04 17:41:26 -0800

    Added sad audio (gary jules mad world)

--- amoeba.js
+++ amoeba.js
@@ -7,7 +7,9 @@ const discord = require('discord.js');
 const client = new discord.Client();
 
 // store the filename of the audio file for sampling later
-const audioFile = "./data/bob-marley-everythings-gonna-be_alright.mp3";
+const happyAudio = "./data/bob-marley-everythings-gonna-be_alright.mp3";
+
+const sadAudio = "./data/gary-jules-mad-world.mp3";
 
 // audio stream options (volume)
 const audioOptions = { volume: 0.3 };
@@ -21,8 +23,6 @@ client.on("message", message => {
    // if the sent message was "ping" then respond with "pong"
    if (message.content === "ping") {
        message.channel.send("pong");
-
-   // if the sent message was "!happy" then play the audio
    } else if (message.content === "!happy") {
 
        // make sure the user is in a voice channel
@@ -33,16 +33,35 @@ client.on("message", message => {
                .then(connection => {
 
                    // play the audio file
-                   const dispatcher = connection.playFile(audioFile, audioOptions);
+                   const dispatcher = connection.playFile(happyAudio, audioOptions);
 
                    // disconnect from the voice channel when the audio is over
                    dispatcher.on("end", () => {
                        message.member.voiceChannel.leave();
-                       fs.appendFileSync("sad-violin-log.txt", moment().format("YYYY-MM-DD HH:mm:ss.SSS ") + audioFile + "\n");
+                       fs.appendFileSync("sad-violin-log.txt", moment().format("YYYY-MM-DD HH:mm:ss.SSS ") +  + "\n");
                    });
                })
                .catch(console.log);
        }
+   } else if (message.content === "!sad") {
+        // make sure the user is in a voice channel
+        if (message.member.voiceChannel) {
+
+            // join the user's voice channel
+            message.member.voiceChannel.join()
+                .then(connection => {
+
+                    // play the audio file
+                    const dispatcher = connection.playFile(sadAudio, audioOptions);
+
+                    // disconnect from the voice channel when the audio is over
+                    dispatcher.on("end", () => {
+                        message.member.voiceChannel.leave();
+                        fs.appendFileSync("sad-violin-log.txt", moment().format("YYYY-MM-DD HH:mm:ss.SSS ") +  + "\n");
+                    });
+                })
+                .catch(console.log);
+        }
    }
 });
 
